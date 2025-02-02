04405bc51d80956a307feea0fcffdd6453bdb5a1
TaRek-ElSayed02 <Tarekswe24@gmail.com>
2025-02-02 11:21:18 +0200

    add hero section with responsive

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import './component/navbar.css';
 import Navbar from './component/Navbar.js';
+import Hero from './component/Hero.js';
+import '../src/component/couch.png'
 
 function App() {
   return (
     <div className="App">
       <div className='container'>
       <Navbar />
+      <Hero title="Modern Interior Design Studio" description="Donec vitae odio quis nisl dapibus malesuada. Nullam ac aliquet velit. Aliquam vulputate velit imperdiet dolor tempor tristique." />
       </div>
     </div>
   );
---
+++ src/component/Hero.js
@@ -0,0 +1,32 @@
+import './hero.css';
+import React, { useState } from "react";
+import couchImage from '../component/couch.png';
+
+
+
+const Hero = (props) => {
+
+    return (
+
+        <div className='hero row justify-content-center align-items-center'>
+            <div className='hero-content col-lg-5 text-left'>
+                    <h1 className='mb-5'>{props.title}</h1>
+                    <p>{props.description}</p>
+                    <div className='hero-buttons'>
+                        <button>Shop Now</button>
+                        <button>Explore</button>
+                    </div>
+            </div>
+            <div className='hero-image col-lg-7'>
+            <img src={couchImage} alt='Hero section image' />
+            </div>
+        </div>
+
+
+    )
+
+
+
+}
+
+export default Hero;
